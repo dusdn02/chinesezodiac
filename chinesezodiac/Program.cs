12a2fc5b829b01daed83b1e1f784e317b7d730df
연우 이 <lyw9114@gmail.com>
2020-05-06 14:32:34 +0900

    chinesezodiac

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,7 +10,48 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
-            int input
+            Console.WriteLine("태어난 년도를 입력해주세요.")
+            int input = int.Parse(Console.ReadLine());
+
+            switch(input % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이 띠");
+                    break;
+                case 1:
+                    Console.WriteLine("닭 띠");
+                    break;
+                case 2:
+                    Console.WriteLine("개 띠");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지 띠");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐 띠");
+                    break;
+                case 5:
+                    Console.WriteLine("소 띠");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이 띠");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼 띠");
+                    break;
+                case 8:
+                    Console.WriteLine("용 띠");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀 띠");
+                    break;
+                case 10:
+                    Console.WriteLine("말 띠");
+                    break;
+                case 11:
+                    Console.WriteLine("양 띠");
+                    break;
+            }
         }
     }
 }
